fe831e3171f332b300de16fa1543e7f47562ac22
Dima Kononenko <dimakononenko@Dimas-MacBook-Pro.local>
2020-10-08 03:34:31 -0700

    (StandartFirmataWifi + Johnny Five + Node) setup

--- mkr1000wifi.js
+++ mkr1000wifi.js
@@ -8,7 +8,7 @@ let firmata = require('firmata')
 // using mkr1000 with WiFi101
 // currently using static ip
 let options = {
-    host: '192.168.86.63',
+    host: '192.168.0.101',
     port: 3030
 }
 
@@ -20,7 +20,7 @@ function blinkLed(led, duration) {
     }, duration)
 }
 
-module.exports = net.connect(options, function() { //'connect' listener
+module.exports = net.connect(options, function () { //'connect' listener
     console.log('connected to server!')
 
     let socketClient = this
@@ -28,13 +28,16 @@ module.exports = net.connect(options, function() { //'connect' listener
     //we can use the socketClient instead of a serial port as our transport
     let io = new firmata.Board(socketClient)
 
-    io.once('ready', function(){
+    io.once('ready', function () {
         console.log('io ready')
         io.isReady = true
 
-        let board = new five.Board({io: io, repl: true})
+        let board = new five.Board({
+            io: io,
+            repl: true
+        })
 
-        board.on('ready', function(){
+        board.on('ready', function () {
             //Full Johnny-Five support here
             console.log('five ready')
 
